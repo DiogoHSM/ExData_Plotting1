2e29468c9c981625d4c06ddb55219274dbffd213
DiogoHSM <diogohsm@gmail.com>
2015-09-13 16:36:56 -0300

    Project 1

---
+++ plot2.R
@@ -0,0 +1,14 @@
+#set a working directory
+setwd("/Users/Diogo/Dropbox/Coursera/4-EDA/ExData_Plotting1")
+
+#read the data
+dataFile <- "./data/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#create the PNG
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
